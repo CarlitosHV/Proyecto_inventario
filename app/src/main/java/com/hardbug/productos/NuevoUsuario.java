682d7f38d33b2ad631d610041a3fb27ca23dbf6d
Kevin Jesús Escutia Ceja <kevinjec115@gmail.com>
2022-11-13 18:35:27 -0600

    Registrar tipo de usuario

--- app/src/main/java/com/hardbug/productos/NuevoUsuario.java
+++ app/src/main/java/com/hardbug/productos/NuevoUsuario.java
@@ -10,17 +10,30 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hardbug.productos.model.UserType;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class NuevoUsuario extends AppCompatActivity {
 
     private static final String TAG = "EmailPassword";
     private FirebaseAuth mAuth;
+
+    FirebaseDatabase firebaseDB;
+    DatabaseReference dbReference;
 
     private com.google.android.material.textfield.TextInputEditText usuario;
     private com.google.android.material.textfield.TextInputEditText contra;
@@ -32,7 +45,7 @@ public class NuevoUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_usuario);
 
-        mAuth = FirebaseAuth.getInstance();
+        iniciarFireBase();
 
         usuario = findViewById(R.id.campousuarioNU);
         contra = findViewById(R.id.contraNU);
@@ -62,13 +75,13 @@ public class NuevoUsuario extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
+                            UserType usuarioNuevo = new UserType(usuario.getText().toString(), "Usuario", user.getUid());
+                            nuevoUsuario(usuarioNuevo);
                             Toast.makeText(NuevoUsuario.this, "Registro Exitoso",
                                     Toast.LENGTH_SHORT).show();
                             finish();
                         } else {
-                            // If sign in fails, display a message to the user.
                             Toast.makeText(NuevoUsuario.this, "Error en el registro",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -76,8 +89,19 @@ public class NuevoUsuario extends AppCompatActivity {
                 });
     }
 
+    private void nuevoUsuario(UserType usuarioNuevo){
+        dbReference.child("UserType").child(usuarioNuevo.getID()).setValue(usuarioNuevo);
+    }
+
     private boolean validarEmail(String email){
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         return pattern.matcher(email).matches();
+    }
+
+    private void iniciarFireBase(){
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDB = FirebaseDatabase.getInstance();
+        dbReference = firebaseDB.getReference();
     }
 }

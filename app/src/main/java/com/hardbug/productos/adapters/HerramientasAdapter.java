9218a4131d62c2acc5e84dda4e8dc216c9e166a2
CarlitosHV <carloshvsa21@hotmail.com>
2022-12-05 20:18:52 -0600

    Categorías se cambiaron a RecyclerView

---
+++ app/src/main/java/com/hardbug/productos/adapters/HerramientasAdapter.java
@@ -0,0 +1,48 @@
+package com.hardbug.productos.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.hardbug.productos.R;
+import com.hardbug.productos.model.Herramientas;
+
+public class HerramientasAdapter extends FirestoreRecyclerAdapter<Herramientas, HerramientasAdapter.ViewHolder> {
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public HerramientasAdapter(@NonNull FirestoreRecyclerOptions<Herramientas> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Herramientas model) {
+        holder.nombre.setText(model.getCode());
+        holder.descripcion.setText(model.getDescripcion());
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cardview_categorias, viewGroup, false);
+        return new HerramientasAdapter.ViewHolder(v);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView nombre, descripcion;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nombre = itemView.findViewById(R.id.nombre_prodcategoria);
+            descripcion = itemView.findViewById(R.id.desc_prodcategoria);
+        }
+    }
+}
